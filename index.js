cb0bc6a92c4d97582fc440a5cc1a7ec7d4283c2c
unknown <ouattara.abraham2019@gmail.com>
2023-03-29 18:35:26 +0100

    ajoute des fontions click menu modification de section profil ect..

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const togglebtni = document.querySelector('.toggle-btn i')
 const menu = document.querySelector('.menu')
 const divClick = document.querySelector('.div-click')
 
+//recuperer l'id des links menu bar a clicker
+const apropos=document.getElementById('apropos')
+const service=document.getElementById('service')
+const contacter=document.getElementById('contacter')
+
+
+
 
 
 
@@ -20,74 +27,26 @@ divClick.onclick =()=> {
     menuIsActive = menu.classList.contains('menu-active')
     togglebtni.classList = menuIsActive? "fa-solid fa-xmark" :
     "fa-solid fa-bars" ;
-  
-
-
-}
-
-
-
-/*
-const creerDiv = ()=>{
-    const div = document.createElement('div')
-    div.id = 'divid'
-    div.classList.add('div-click')
-
-    document.body.appendChild(div)
-} 
-togglebtn.onclick =()=> {
+  }
+  apropos.onclick =()=> {
     menu.classList.toggle('menu-active')
+    divClick.classList.toggle('div-active')
     menuIsActive = menu.classList.contains('menu-active')
-    if (menuIsActive ) {
-        togglebtni.classList = "fa-solid fa-xmark" ;
-        creerDiv() 
-    } else{
-        togglebtni.classList =  "fa-solid fa-bars" ;
-       
-       
-       
-    }  */
-    /*
-    menu.classList.toggle('menu-active')
-    menuIsActive = menu.classList.contains('menu-active')
-    togglebtni.classList = menuIsActive ?  "fa-solid fa-xmark" :
+    togglebtni.classList = menuIsActive? "fa-solid fa-xmark" :
     "fa-solid fa-bars" ;
-    creerDiv()  
-    */
-
-
-
-
-/* const nouvellelDiv = ()=>{
-    togglebtni.classList= "fa-solid fa-xmark" 
- const div = document.createElement('div') ;
- div.id = 'iddiv'
- div.className='nouvelle-div' ;
- document.body.appendChild(div)
-
-
-}
-const removeNouvelleDiv =()=>{
-    togglebtni.classList= "fa-solid fa-bars" ;
-  div.remove()
-}
-
-togglebtn.onclick=()=>{
+  }
+  service.onclick =()=> {
     menu.classList.toggle('menu-active')
-    btnActive=menu.classList.contains('menu-active')
-    if (btnActive) {
-        nouvellelDiv();
-        
-    } else{
-        removeNouvelleDiv()
-    }
-   
-}
+    divClick.classList.toggle('div-active')
+    menuIsActive = menu.classList.contains('menu-active')
+    togglebtni.classList = menuIsActive? "fa-solid fa-xmark" :
+    "fa-solid fa-bars" ;
+  }
+  contacter.onclick =()=> {
+    menu.classList.toggle('menu-active')
+    divClick.classList.toggle('div-active')
+    menuIsActive = menu.classList.contains('menu-active')
+    togglebtni.classList = menuIsActive? "fa-solid fa-xmark" :
+    "fa-solid fa-bars" ;
+  }
 
-document.addEventListener('click',( (e)=>{
-   if (e.target.nodeName=='DIV') {
-    console.log('target name div ');
-   }
-}
-
-)) */
